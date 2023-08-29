211163a42d715258e79868d01c05a250c6625814
Michal Backiel <mback@softserveinc.com>
2023-08-29 17:12:03 +0200

    add FilterByCommonName

---
+++ Extensions/CountryExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UseCase1.Models;
+
+namespace UseCase1.Extensions
+{
+    public static class CountryExtensions
+    {
+        public static List<Country> FilterByCommonName(this List<Country> countries, string str)
+        {
+            return countries.Where(x => x.Name?.Common?.Contains(str, StringComparison.InvariantCultureIgnoreCase) ?? false).ToList();
+        }
+    }
+}
