aa85f85e8eee5f51178a7729f0c51c8f4d64ad1f
Michal Backiel <mback@softserveinc.com>
2023-08-29 17:19:57 +0200

    add FilterByPopulation

--- Extensions/CountryExtensions.cs
+++ Extensions/CountryExtensions.cs
@@ -8,5 +8,11 @@ namespace UseCase1.Extensions
         {
             return countries.Where(x => x.Name?.Common?.Contains(str, StringComparison.InvariantCultureIgnoreCase) ?? false).ToList();
         }
+
+        public static List<Country> FilterByPopulation(this List<Country> countries, int maxPopulationInMilions)
+        {
+            int maxPopulation = maxPopulationInMilions * 1000000;
+            return countries.Where(x => x.Population < maxPopulation).ToList();
+        }
     }
 }
